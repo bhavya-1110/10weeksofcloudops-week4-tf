0c14b8e2845a2ad0708d9135459f345f131097d9
bhavya_py_git <bhavya.shah1110@gmail.com>
2026-01-06 19:41:58 +0530

    bucket

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "10weeksofcloudops-week4"
+    bucket = "demowebapp29"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
     dynamodb_table = "10weeksofcloudops-dynamo"
